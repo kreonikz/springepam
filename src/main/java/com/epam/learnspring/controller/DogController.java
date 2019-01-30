a636046a7e007e8a200e6935e43f30f8df832e35
Alexey <taler2002@gmail.com>
2019-01-30 17:13:23 +0300

    3

--- src/main/java/com/epam/learnspring/controller/DogController.java
+++ src/main/java/com/epam/learnspring/controller/DogController.java
@@ -1,10 +1,8 @@
 package com.epam.learnspring.controller;
 
 import com.epam.learnspring.model.Dog;
-import com.epam.learnspring.model.Message;
 import com.epam.learnspring.service.DogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -16,9 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/dog")
 public class DogController {
     @Autowired
-    @Value("DOGGGG")
-    private Message message;
-    @Autowired
     DogService dogService;
 
     private ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ioc.xml");
@@ -26,8 +21,9 @@ public class DogController {
 
     @GetMapping
     public String getDog(Model model) {
-        model.addAttribute("name", dog1.getName());
-//        model.addAttribute("name", dogService.getName());
+        model.addAttribute("nameFromXml", dog1.getName());
+        model.addAttribute("name", dogService.getName());
+
         return "dog";
     }
 }
