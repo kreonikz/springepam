bd91d88d5769520fc762257edfc7612a2f70f008
Alexey <taler2002@gmail.com>
2019-02-08 17:13:50 +0300

    5

---
+++ src/main/java/com/epam/learnspring/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+//package com.epam.learnspring.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@EnableWebSecutrity
+//public class SecurityConfig {
+//
+//
+//}
