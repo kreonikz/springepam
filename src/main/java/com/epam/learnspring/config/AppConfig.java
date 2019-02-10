87805ee5d8adfa96ad7c175d91d95397597fa5fe
Alexey <taler2002@gmail.com>
2019-02-10 21:55:09 +0300

    added access to DB

--- src/main/java/com/epam/learnspring/config/AppConfig.java
+++ src/main/java/com/epam/learnspring/config/AppConfig.java
@@ -1,16 +1,9 @@
 package com.epam.learnspring.config;
 
-import com.epam.learnspring.model.Dog;
-import com.epam.learnspring.model.Message;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
 
-//    @Bean
-//    public Message message() {
-//        return new Message("cat is sleep...");
-//    }
 }
--- src/main/java/com/epam/learnspring/controller/CatController.java
+++ src/main/java/com/epam/learnspring/controller/CatController.java
@@ -2,9 +2,7 @@ package com.epam.learnspring.controller;
 
 import com.epam.learnspring.model.Cat;
 import com.epam.learnspring.service.CatService;
-import com.epam.learnspring.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -13,9 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CatController {
-//    @Autowired
-//    @Value("Murzik")
-//    private Message message;
 
     @Autowired
     private CatService catService;
