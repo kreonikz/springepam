5f76be1894b09425b1138f7c7a2b281b3795c78f
Alexey <taler2002@gmail.com>
2019-02-10 16:03:21 +0300

    6

--- src/main/java/com/epam/learnspring/config/SecurityConfig.java
+++ src/main/java/com/epam/learnspring/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 //package com.epam.learnspring.config;
 //
 //import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //
 //@Configuration
 //@EnableWebSecurity
-//public class SecurityConfig (Auth) {
+//@EnableGlobalMethodSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
-//
+//    public void configere(AuthenticationManagerBuilder auth) throws Exception {
+//    auth.userDetailsService().
+//    }
 //}
