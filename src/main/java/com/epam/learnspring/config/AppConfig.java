2f3753512eda5fc90c56b7860481753f76e2fc58
michael Kolbasov <trololoshenkilolo@yandex.ru>
2019-02-13 17:36:13 +0300

    dev-kolbasov: hibernate

--- src/main/java/com/epam/learnspring/config/AppConfig.java
+++ src/main/java/com/epam/learnspring/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.epam.learnspring.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
 public class AppConfig {
 
 
--- src/main/java/com/epam/learnspring/service/KnightService.java
+++ src/main/java/com/epam/learnspring/service/KnightService.java
@@ -1,11 +1,19 @@
 package com.epam.learnspring.service;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KnightService {
+    private SessionFactory sessionFactory;
 
     public String getAchievement(String value) {
         return "I've slayed a " + value + "!";
+    }
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 }
